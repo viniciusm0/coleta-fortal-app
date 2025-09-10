2df81b87b086856a71fe0ead1da88150bf55b502
Vinicius Moreira <viniciusm1603@gmail.com>
2025-09-10 19:11:22 -0300

    Adicionado Maps

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import MapView from 'react-native-maps';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Meu deus</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  }
+})
