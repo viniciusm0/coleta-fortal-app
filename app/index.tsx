70cd762b7652c49e7a21254e59e19c8bdf15e9eb
Vinicius Moreira <viniciusm1603@gmail.com>
2025-09-10 19:30:49 -0300

    feat(nativewind): adicionado nativewind com react-native-maps

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import MapView from 'react-native-maps';
+import "../global.css";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
+    <View className="flex w-screen h-screen items-center justify-center z-10">
+      <Text className="text-xl font-bold text-blue-500">
+        Mapa com nativewind integrado
+      </Text>
+      <MapView style={{width: '100%', height: '100%', zIndex: -10, position: 'absolute'}}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  map: {
-    width: '100%',
-    height: '100%',
-  }
-})
