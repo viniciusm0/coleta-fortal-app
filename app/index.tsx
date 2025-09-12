c2cce8fa3d85d39b534d6e9a563d79a1ce49692d
Vinicius Moreira <viniciusm1603@gmail.com>
2025-09-12 13:56:07 -0300

    App working

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
   return (
     <View className="flex w-screen h-screen items-center justify-center z-10">
       <Text className="text-xl font-bold text-blue-500">
-        Mapa com nativewind integrado
+        Mapa com nativewind
       </Text>
       <MapView style={{width: '100%', height: '100%', zIndex: -10, position: 'absolute'}}/>
     </View>
