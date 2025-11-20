dd07191360f8dbcd4a40d12371ffa2510c0f1fc6
mauriciomarques34 <mauriciocamarques@gmail.com>
2025-11-20 16:22:46 -0300

    Telaajudaadicionada

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,9 @@
 import DrawerRoutes from "@/src/components/Routes/Drawer.routes";
 
-console.log(">>> DrawerRoutes é:", DrawerRoutes);
 
 export default function RootLayout() {
-  console.log(">>> RootLayout carregou");
   return (
-   
-       <DrawerRoutes />
+    <DrawerRoutes />
     
   );
 }
